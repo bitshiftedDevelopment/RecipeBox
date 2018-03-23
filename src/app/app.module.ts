d64022c8ac4171a6005368b43d050f89b65ceb04
Mick Evans <bitshifted.development@gmail.com>
2018-03-23 18:26:38 -0400

    Added Material Design, Material Icons, and Font Awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialAppModule } from './ngmat/ngmat.module';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialAppModule
   ],
   providers: [],
   bootstrap: [AppComponent]
